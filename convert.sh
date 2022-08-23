cfeb7477ebe14144b8814299ac4d62f41059eb07
Kristien Harris <harris.kristien@gmail.com>
2022-08-23 12:11:25 +0100

    Added filepath so can run this on a file not in the script directory

--- convert.sh
+++ convert.sh
@@ -3,16 +3,17 @@ echo 'This script converts an image to a specified file format'
 read -p 'Enter filename to convert: ' file
 read -p 'Enter desired file formats: ' format
 
+filepath="$PWD/$file"
 filename="${file%.*}"
 extension="${file##*.}"
 
 for i in ${format[@]}; do
     if [ "${format[$i]}" = "base64" ]; then
-        cat $file | base64 -w 0 > $filename"_"$i"."txt
+        cat "$filepath" | base64 -w 0 > $filename"_"$i"."txt
         echo "Conversion complete" $filename"_"$i"."txt
     else
         newFile=$filename"."$i
-        magick $file $newFile
+        magick "$filepath" $newFile
         echo "Conversion complete" $newFile
     fi
 done
